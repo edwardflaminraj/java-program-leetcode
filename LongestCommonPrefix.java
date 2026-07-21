df71c60923c530d3aaf3c1e8c8f5234be5039706
edwardflaminraj <edwardflaminraj2006@gmail.com>
2026-07-21 11:27:26 +0530

    String Method

---
+++ LongestCommonPrefix.java
@@ -0,0 +1,11 @@
+public class LongestCommonPrefix{
+    public String longestCommonPrefix(String[] strs) {
+        String prefix=strs[0];
+        for(int i=0;i<strs.length;i++){
+            while(!strs[i].startsWith(prefix)){
+                prefix=prefix.substring(0,prefix.length()-1);
+            }
+        }
+        return prefix;
+    }
+}
